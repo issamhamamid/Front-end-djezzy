6d395dadc763673b90d29fb294449759647ccaee
issamhamamid <issamhamamid6@gmail.com>
2023-03-02 07:01:58 +0100

    22/02/2023

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
                     </PrivateRoute> ) :
                         (
                             <PrivateRoute>
-                                <Navbaar/>
+                                <Home/>
                             </PrivateRoute>
 
                         )
